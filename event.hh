50010d8074384f7b389bc402386a7392bd3145f6
Kevin Lohman <badpirate@gmail.com>
2019-01-13 15:38:24 -0800

    responsive

--- event.hh
+++ event.hh
@@ -56,54 +56,58 @@ function guestRow(GuestModel $guest, bool $paid) : :xhp {
   $paid_enabled = "$paid_shared btn-success";
   $paid_disabled = "$paid_shared";
 
-  $divvy_shared = "btn divvy-watch col-auto";
+  $divvy_shared = "btn divvy-watch";
   $divvy_enabled = "$divvy_shared btn-primary";
   $divvy_disabled = "$divvy_shared";
 
   return
-    <li class="list-group-item d-flex">
+    <div class="bg-white mt-1 p-1">
       <input type="hidden" name="guest_id[]" value={"".$guest->id}/>
       <input type="hidden" name="paid[]" value={$paid ? '1' : '0'} 
        class="hidden-paid" id={"hidden-paid-$guest->id"}/>
       <input type="hidden" name="divvy[]" value="1" id={"hidden-divvy-$guest->id"}
        class="hidden-divvy"/>
-      <span class="col-auto mr-auto">{$guest->name}</span>
-      <div class="btn-group ml-1">
-        <button type="button" class={$paid ? $paid_enabled : $paid_disabled}
-         id={"button-paid-$guest->id"} onclick={"
-           $('.hidden-paid').val('0');
-           $('.button-paid').attr('class','$paid_disabled');
-           $('#hidden-paid-$guest->id').val('1');
-           $('#button-paid-$guest->id').attr('class','$paid_enabled');
-         "}>
-          Paid
-        </button>
+      <div class="h5">
+        {$guest->name}
+        <span class="ml-1 h5 display-divvy col-2">$0.00</span>
       </div>
-      <div class="btn-group d-flex col-auto">
-        <button type="button" class={$divvy_enabled} id={"button-divvy-$guest->id"}
-         onclick={"
-          $('#button-exclude-$guest->id').attr('class','$divvy_disabled');
-          $('#hidden-divvy-$guest->id').val('1');
-          $('#button-divvy-$guest->id').attr('class','$divvy_enabled');
-          calculate();
-         "}>
-          Divvy
-        </button>
-        <button type="button" class={$divvy_disabled} id={"button-exclude-$guest->id"}
-         onclick={"
-          $('#button-exclude-$guest->id').attr('class','$divvy_enabled');
-          $('#hidden-divvy-$guest->id').val('0');
-          $('#button-divvy-$guest->id').attr('class','$divvy_disabled');
-          calculate();
-         "}>
-           Exclude
-        </button>
+      <div class="row m-0">
+        <div class="btn-group mr-auto">
+          <button type="button" class={$paid ? $paid_enabled : $paid_disabled}
+          id={"button-paid-$guest->id"} onclick={"
+            $('.hidden-paid').val('0');
+            $('.button-paid').attr('class','$paid_disabled');
+            $('#hidden-paid-$guest->id').val('1');
+            $('#button-paid-$guest->id').attr('class','$paid_enabled');
+          "}>
+            Paid
+          </button>
+        </div>
+        <div class="btn-group">
+          <button type="button" class={$divvy_enabled} id={"button-divvy-$guest->id"}
+          onclick={"
+            $('#button-exclude-$guest->id').attr('class','$divvy_disabled');
+            $('#hidden-divvy-$guest->id').val('1');
+            $('#button-divvy-$guest->id').attr('class','$divvy_enabled');
+            calculate();
+          "}>
+            Divvy
+          </button>
+          <button type="button" class={$divvy_disabled} id={"button-exclude-$guest->id"}
+          onclick={"
+            $('#button-exclude-$guest->id').attr('class','$divvy_enabled');
+            $('#hidden-divvy-$guest->id').val('0');
+            $('#button-divvy-$guest->id').attr('class','$divvy_disabled');
+            calculate();
+          "}>
+            Exclude
+          </button>
+        </div>
       </div>
-      <span class="ml-1 h5 display-divvy col-2">$0.00</span>
-    </li>;
+    </div>;
 }
 
-$coming_xhp = <ul class="list-group"/>;
+$coming_xhp = <div/>;
 
 foreach($event->guests as $guest) {
   $coming_xhp->appendChild(guestRow($guest, $guest->id === $event->primary->id));
@@ -123,9 +127,19 @@ foreach($event->guests as $guest) {
 $guest_xhp->appendChild(<span class="col"/>); // Button column
 
 $transaction_list_xhp = 
-  <ul class="list-group">
+  <ul class="list-group d-none d-md-block">
     {$guest_xhp}
   </ul>;
+
+$transaction_list_compact_rows = <tbody/>;
+
+
+$transaction_list_compact_xhp = 
+  <table class="table table-striped d-md-none">
+    {$transaction_list_compact_rows}
+  </table>;
+
+$transaction_list_compact_summary_xhp = <div class="d-md-none"/>;
 
 $txs = TransactionModel::forEvent($event->id);
 
@@ -147,7 +161,8 @@ function payButton(
       <input type="hidden" name="button_subtype" value="services"/>
       <input type="hidden" name="no_note" value="0"/>
       <input type="hidden" name="bn" value="PP-BuyNowBF:btn_paynow_SM.gif:NonHostedGuest"/>
-      <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynow_SM.gif" name="submit" alt="PayPal - The safer, easier way to pay online!"/>
+      <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynow_SM.gif" name="submit" 
+       alt="PayPal - The safer, easier way to pay online!"/>
     </form>;
 }
 
@@ -159,14 +174,20 @@ if (count($txs)) {
         <span class="col">
           {$tx->description} (${ $tx->total() })</span>
       </li>;
+    $payer_names = [];
     foreach($event->guests as $guest) {
       $total = 0;
       $is_payee = $tx->payee_id === $guest->id;
       if ($tx->payer_ids->linearSearch($guest->id) !== -1) {
         $total -= $tx->amount;
+        $payer_names[] = $guest->name;
       }
       if ($is_payee) {
-        $total -= $tx->payee_paid ? $tx->amount : 0;
+        if ($tx->payee_paid) {
+          $total -= $tx->amount;
+          $payer_names[] = $guest->name;
+        }
+        $payee_name = $guest->name;
       }
       $transaction_row_xhp->appendChild(
         <span class="col">
@@ -181,6 +202,15 @@ if (count($txs)) {
         ? $tx_totals[$guest->id] + $total 
         : $total;
     }
+    $transaction_list_compact_rows->appendChild(
+      <tr>
+        <td>
+          {count($payer_names) > 1
+           ? implode(', ',$payer_names)." split the cost ($".$tx->total().") of $tx->description.  $payee_name paid."
+           : "$payee_name paid ".$payer_names[0]." $$tx->amount."}
+        </td>
+      </tr>
+    );
     $transaction_row_xhp->appendChild(
       <form method="post" class="col">
         <input type="hidden" name="tx_id" value={$tx->transaction_id}/>
@@ -213,32 +243,47 @@ if (count($txs)) {
       $bg = "bg-danger";
       $payed_button = !$guest_me && $owed > 0;
     }
+    $payed_button_xhp = $payed_button ?
+      <div>
+        <form method="post" class="d-flex">
+          <input type="hidden" name="guest_id[]" value={$g}/>
+          <input type="hidden" name="paid[]" value="0"/>
+          <input type="hidden" name="divvy[]" value="1"/>
+          <input type="hidden" name="guest_id[]" value={$guest->id}/>
+          <input type="hidden" name="paid[]" value="1"/>
+          <input type="hidden" name="divvy[]" value="0"/>
+          <input type="hidden" name="text-amount" value={abs($owe)}/>
+          <input type="hidden" name="text-description" value={
+            "$guest->name paid up"
+          }/>
+          <input class="btn col-12" type="submit" value="Paid me"/>
+        </form>
+      </div> : null;
+
+    $pay_button_xhp = 
+      ($tx_totals[$g] < 0 && $pay && !$guest_me)
+      ? payButton(
+          $event->title,
+          $event->id,
+          $guest->email,
+          $owe < abs($owed) ? $owe : abs($owed))
+      : null;
+
+    $transaction_list_compact_summary_xhp->appendChild(
+      <div class={"$bg text-light p-3"}>
+        <span class="mr-auto">
+          {"$guest->name $owes $".abs(round($owe,2))}
+        </span>
+        { $pay_button_xhp }
+        { $payed_button_xhp }
+      </div>
+    );
+
     $totals_row_xhp->appendChild(
       <div class={"col $bg text-white"}>
         {$owes} ${abs(round($owe,2))}
-        {($tx_totals[$g] < 0 && $pay && !$guest_me)
-        ? payButton(
-            $event->title,
-            $event->id,
-            $guest->email,
-            $owe < abs($owed) ? $owe : abs($owed))
-        : null}
-        {$payed_button
-         ?  <div>
-              <form method="post">
-                <input type="hidden" name="guest_id[]" value={$g}/>
-                <input type="hidden" name="paid[]" value="0"/>
-                <input type="hidden" name="divvy[]" value="1"/>
-                <input type="hidden" name="guest_id[]" value={$guest->id}/>
-                <input type="hidden" name="paid[]" value="1"/>
-                <input type="hidden" name="divvy[]" value="0"/>
-                <input type="hidden" name="text-amount" value={abs($owe)}/>
-                <input type="hidden" name="text-description" value={
-                  "$guest->name paid up"
-                }/>
-                <input class="btn" type="submit" value="Paid me"/>
-              </form>
-            </div> : null}</div>
+        {$pay_button_xhp}
+        { $payed_button_xhp }</div>
     );
   }
   $totals_row_xhp->appendChild(<span class="col"/>);
@@ -249,8 +294,10 @@ if (count($txs)) {
       <div class="card-header h6">
         Transactions
       </div>
-      <div class="card-body">
+      <div class="card-body p-0 p-md-3">
         {$transaction_list_xhp}
+        {$transaction_list_compact_xhp}
+        {$transaction_list_compact_summary_xhp}
       </div>
     </div>;
 }
@@ -281,11 +328,11 @@ $url = getenv('DIVVY_SITE')."/event.hh?id=$event->id";
 
 print 
   <html>
-    <head:jstrap reactive={false}>
+    <head:jstrap>
       <script src="js/divvy.js"></script>
       <title>Divvy {$event->title}!</title>
     </head:jstrap>
-    <body class="container" style="width: 970px !important">
+    <body class="container">
       <div>
         <button class="alert alert-info h5 w-100" onclick={"
           var temp = $('<input>');
@@ -317,13 +364,13 @@ print
             Divvy up your trip: {$event->title}
           </h5>
         </div>
-        <div class="card-body">
+        <div class="card-body p-0 pt-3 p-md-3">
           { count($txs) > 0 ? $transactions_xhp : null }
           <div class="card bg-info">
             <div class="card-header h6 text-white">
               Add an expense
             </div>
-            <div class="card-body">
+            <div class="card-body p-0 pt-3 p-md-3">
               <form method="post">
                 <input type="hidden" name="id" value={$_REQUEST['id']}/>
                 <input type="text" class="form-control" placeholder="Describe transaction" 
